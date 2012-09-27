0aff6b79b4c2fe37097fabad31162130173c3c95
Blue112 <bluesansdouze@gmail.com>
2012-09-27 19:38:27 +0200

    First kind of playable version

--- src/eu/blue112/arttp/Game.hx
+++ src/eu/blue112/arttp/Game.hx
@@ -1,14 +1,53 @@
 package eu.blue112.arttp;
 
 import flash.display.Sprite;
+import flash.events.KeyboardEvent;
+
+import eu.blue112.arttp.engine.RhythmManager;
 
 class Game extends Sprite
 {
+	static public inline var GAME_WIDTH:Int = 800;
+	static public inline var GAME_HEIGHT:Int = 600;
+
+	var manager:RhythmManager;
+
 	public function new()
 	{
 		super();
 
 		flash.Lib.current.addChild(this);
+
+		stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		stage.align = flash.display.StageAlign.TOP_LEFT;
+
+		manager = new RhythmManager(1000);
+
+		trace("Appuyez sur espace toutes les secondes !");
+
+		stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+	}
+
+	private function onKeyDown(e:KeyboardEvent):Void
+	{
+		if (e.keyCode == flash.ui.Keyboard.SPACE)
+		{
+			if (!manager.is_inited())
+			{
+				manager.init();
+				trace("Encore une fois pour démarrer.");
+			}
+			else
+			{
+				var result = manager.check();
+				switch (result)
+				{
+					case GOOD: trace("Bien !");
+					case TOO_SOON(diff): trace("Trop rapide de "+diff+" ms !");
+					case TOO_LATE(diff): trace("Trop lent de "+diff+" ms !");
+				}
+			}
+		}
 	}
 
 	static public function main()
---
+++ src/eu/blue112/arttp/engine/RhythmManager.hx
@@ -0,0 +1,57 @@
+package eu.blue112.arttp.engine;
+
+enum RhythmResult
+{
+	GOOD;
+	TOO_LATE(diff:Int);
+	TOO_SOON(diff:Int);
+}
+
+class RhythmManager
+{
+	var last_time:Float;
+
+	var tolerance:Int;
+	var tick:Int;
+
+	public function new(tick:Int, ?tolerance:Int = 50)
+	{
+		last_time = 0;
+
+		this.tick = tick;
+		this.tolerance = tolerance;
+	}
+
+	public function is_inited()
+	{
+		return last_time != 0;
+	}
+
+	public function init()
+	{
+		last_time = haxe.Timer.stamp();
+	}
+
+	public function check()
+	{
+		var diff = Std.int((haxe.Timer.stamp() - last_time) * 1000);
+		last_time = haxe.Timer.stamp();
+
+		if (diff + tolerance >= tick)
+		{
+			if (diff - tolerance <= tick)
+			{
+				return GOOD;
+			}
+			else
+			{
+				return TOO_SOON(diff - (tick + tolerance));
+			}
+		}
+		else
+		{
+			return TOO_LATE((tick - tolerance) - diff);
+		}
+
+	}
+}
